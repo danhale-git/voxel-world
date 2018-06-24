7dd0a0d64be9d7d266ba52d4de74b3ede77805a5
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2018-06-24 17:19:52 +0100

    grid spiral code

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -12,4 +12,38 @@ public static class Util
     	TimeSpan span = current - dt1970;
     	return span.TotalMilliseconds;
 	}
+
+	public static void SpiralTest()
+	{
+		Vector3 position = new Vector3(0,50,0);
+		int increment = 1;
+		for(int i = 0; i < 10; i++)
+		{
+			for(int r = 0; r < increment; r++)
+			{
+				position += Vector3.right;
+				//CreateCube(position);
+			}
+			for(int d = 0; d < increment; d++)
+			{
+				position += Vector3.back;
+				//CreateCube(position);
+			}
+
+			increment++;
+
+			for(int l = 0; l < increment; l++)
+			{
+				position += Vector3.left;
+				//CreateCube(position);
+			}
+			for(int u = 0; u < increment; u++)
+			{
+				position += Vector3.forward;
+				//CreateCube(position);
+			}
+
+			increment++;
+		}
+	}
 }
