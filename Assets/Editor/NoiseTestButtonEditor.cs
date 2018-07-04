d73f31695bfbb92bf6e4f9cc1c9604d43e68948a
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2018-07-04 20:11:52 +0100

    more advanced noise testing

---
+++ Assets/Editor/NoiseTestButtonEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(NoiseTesting))]
+public class NoiseTestButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        NoiseTesting myScript = (NoiseTesting)target;
+        if(GUILayout.Button("Make noise"))
+        {
+            myScript.Noise();
+        }
+    }
+}
--- Assets/Scripts/NoiseTesting.cs
+++ Assets/Scripts/NoiseTesting.cs
@@ -2,40 +2,70 @@
 using System.Collections;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class NoiseTesting : MonoBehaviour
 {
 	private Texture2D texture;
     private SpriteRenderer spriteRenderer;
 	private RectTransform rectTrans;
 
-	int size = 512;
+	public enum Algorithm {Brownian, Perlin}
+
+	
+	public Algorithm algorithm;
+	public int gridSize;
+
+	public int size = 1024;
+	public float frequency;
+
+	[Header("Brownian only")]
+	public int octaves;
+	public float persistence;
+
+	public float multiplyX;
+	public float multiplyY;
+
+	public float redMin;
+	public float redMax;
 
     void Start()
 	{
         spriteRenderer = GetComponent<SpriteRenderer>();
 		rectTrans = GetComponent<RectTransform>();
 		//size = (int)rectTrans.rect.height;
-		texture = new Texture2D(size, size);
 		Noise();
     }
-    
-	void Noise()
+
+	public float GetPixelNoise(int x, int y)
 	{
+		switch(algorithm)
+		{
+			case Algorithm.Brownian:
+				return NoiseUtils.BrownianMotion((x*frequency)*multiplyX, (y*frequency)*multiplyY, octaves, persistence);
+			case Algorithm.Perlin:
+				return Mathf.PerlinNoise((x*frequency)*multiplyX, (y*frequency)*multiplyY);
+			default:
+				return 0;
+		}
+	}
+    
+	public void Noise()
+	{
+		texture = new Texture2D(size, size);
 		Debug.Log("Generating noise");
 		for(int x = 0; x < size; x++)
 			for(int y = 0; y < size; y++)
 			{
-				float factor = 0.01f;
-				float perlin = NoiseUtils.BrownianMotion(x*factor, y*factor, 1, 0.3f);
-
+				float noise = GetPixelNoise(x, y) ;
+				
 				int _x = x;// - x/2;
 				int _y = y;// - y/2;
-
-				texture.SetPixel(_x, _y, new Color(perlin, perlin, perlin, 1));
+				
+				texture.SetPixel(_x, _y, new Color(noise, noise, noise, 1));
+				if(noise < redMax && noise > redMin) texture.SetPixel(_x, _y, Color.red);
+				else if(x % gridSize == 0 || y % gridSize == 0) texture.SetPixel(_x, _y, Color.green);
 			}
 		
-		Debug.Log(rectTrans.pivot);
-
 		texture.Apply();
 		Sprite mySprite = Sprite.Create(texture, new Rect(0, 0, size, size), new Vector3(0.5f, 0.5f));
 		mySprite.name = "noiseImage";
@@ -45,5 +75,17 @@ public class NoiseTesting : MonoBehaviour
 	void Update()
 	{
 		if(Input.GetKeyDown(KeyCode.Space)) Noise();
+	}
+
+	class ObjectBuilderScript : MonoBehaviour 
+	{
+		public GameObject obj;
+		public Vector3 spawnPoint;
+
+		
+		public void BuildObject()
+		{
+			Instantiate(obj, spawnPoint, Quaternion.identity);
+		}
 	}
 }
