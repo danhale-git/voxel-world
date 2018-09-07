981dc062e875bdc29e23ded310488d531376a27f
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2018-09-07 16:37:47 +0100

    Much more readable code

--- Assets/Scripts/World/LSystem.cs
+++ Assets/Scripts/World/LSystem.cs
@@ -14,6 +14,20 @@ public class LSystem
 	List<Zone.Side> originSides = new List<Zone.Side>();
 	List<int[]> allBounds = new List<int[]>();
 
+	int right = 0;
+	int left = 1;
+	int front = 2;
+	int back = 3;
+
+	Int2 forward = new Int2(0,1);
+
+	Zone.Side currentBack = Zone.Side.BOTTOM;
+
+
+
+	
+
+	
 	public LSystem(PointOfInterest POI, Zone zone, float noise)
 	{
 		this.POI = POI;
@@ -26,13 +40,95 @@ public class LSystem
 		//noiseGen.SetSeed(7425356); works
 		//noiseGen.SetSeed(85646465);
 		//noiseGen.SetSeed(9434);
+		//noiseGen.SetSeed(1114);
+		//noiseGen.SetSeed(5281);	
+		//noiseGen.SetSeed(6999);	
+		noiseGen.SetSeed(4727);		
+
 		
-		int seed = Random.Range(0,10000);
-		Debug.Log("SEED: "+ seed);
-		noiseGen.SetSeed(seed);
+		//int seed = Random.Range(0,10000);
+		//Debug.Log("SEED: "+ seed);
+		//noiseGen.SetSeed(seed);
 
 		DrawBlockMatrix();
 	}
+
+# region Rotation
+
+	int ForwardAxis(Int2 point)
+	{
+		if(right == 0 || right == 1) return point.z;
+		else return point.x;
+	}
+	int SideAxis(Int2 point)
+	{
+		if(right == 0 || right == 1) return point.x;
+		else return point.z;
+	}
+
+	bool ToLeft(int compare, int to)
+	{
+		if(currentBack == Zone.Side.BOTTOM || currentBack == Zone.Side.RIGHT)
+			return (compare < to);
+		else return (compare > to);
+	}
+	bool ToRight(int compare, int to)
+	{
+		if(currentBack == Zone.Side.BOTTOM || currentBack == Zone.Side.RIGHT)
+			return (compare > to);
+		else return (compare < to);
+	}
+	bool InFront(int compare, int to)
+	{
+		if(currentBack == Zone.Side.BOTTOM || currentBack == Zone.Side.LEFT)
+			return (compare > to);
+		else return (compare < to);
+	}
+	bool Behind(int compare, int to)
+	{
+		if(currentBack == Zone.Side.BOTTOM || currentBack == Zone.Side.LEFT)
+			return (compare < to);
+		else return (compare > to);
+	}
+
+	void Rotate(Zone.Side backSide)
+	{
+		currentBack = backSide;
+		switch(backSide)
+		{	
+			case Zone.Side.RIGHT:
+				right = 2;
+				left = 3;
+				front = 1;
+				back = 0;
+				forward = new Int2(-1, 0);
+				break;
+			case Zone.Side.LEFT:
+				right = 3;
+				left = 2;
+				front = 0;
+				back = 1;
+				forward = new Int2(1, 0);
+				break;
+			case Zone.Side.TOP:
+				right = 1;
+				left = 0;
+				front = 3;
+				back = 2;
+				forward = new Int2(0, -1);
+				break;
+			case Zone.Side.BOTTOM:
+				right = 0;
+				left = 1;
+				front = 2;
+				back = 3;
+				forward = new Int2(0, 1);
+				break;
+
+		}
+	}
+
+# endregion
 
 
 	void DrawBlockMatrix()
@@ -40,8 +136,7 @@ public class LSystem
 		int width = zone.size * World.chunkSize;
 		int height = zone.size * World.chunkSize;
 
-		Int2 startPoint = RandomPointAtEdge(zone.back, width, height, noise);
-		Vector3 startGlobal = MatrixToGlobal(startPoint);
+		Int2 startPoint = RandomPointOnSide((int)zone.back, zone.bounds);
 		noise = noiseGen.GetNoise01(startPoint.x, startPoint.z);
 
 
@@ -50,7 +145,7 @@ public class LSystem
 		zone.blockMatrix = new int[width,height];
 		zone.blockMatrix[startPoint.x,startPoint.z] = 1;
 
-		GenerateBuilding(startPoint, zone.back, 16, 4);
+		GenerateBuilding(startPoint, zone.back, 10, 5);
 
 		foreach(int[] room in allBounds)
 		{
@@ -78,15 +173,18 @@ public class LSystem
 				break;
 			}
 
-			//	out keyword for new point and new side?
 			bool[] eligibleSides;
 
-			int[] newBounds = GenerateRoom(originSides[i], originPoints[i], i, out eligibleSides);
+			int[] newBounds = Room(originSides[i], originPoints[i], i, out eligibleSides);
 
 			int boundsWidth = newBounds[0] - newBounds[1];
 			int boundsHeight = newBounds[2] - newBounds[3];
 
-			if(boundsWidth < minSize || boundsHeight < minSize) break;
+
+			if(boundsWidth < minSize || boundsHeight < minSize)
+			{
+				break;
+			}
 			else
 			{
 				allBounds.Add(newBounds);
@@ -95,11 +193,100 @@ public class LSystem
 				originSides.Add(Zone.Opposite((Zone.Side)chosenSide));
 			}
 		}
-
-		Debug.Log(iterations);
-
 	}
 
+	int[] Room(Zone.Side backSide, Int2 originPoint, int index, out bool[] eligibleSides)
+	{
+		Rotate(backSide);
+
+		int rightAdjacent;
+		int leftAdjacent;
+
+		int[] bounds = new int[4];
+
+		bool adjacent = CheckAdjacent(originPoint, index, out rightAdjacent, out leftAdjacent);
+
+		bounds[right] = rightAdjacent == 0 ? RandomRange(SideAxis(originPoint), zone.bounds[right]) : rightAdjacent;
+		bounds[left] = leftAdjacent == 0 ? RandomRange(SideAxis(originPoint), zone.bounds[left]) : leftAdjacent;
+
+		int closestInFront;
+
+		if(CheckForward(originPoint, bounds, index, out closestInFront))
+			bounds[front] = closestInFront;
+		else
+			bounds[front] = RandomRange(ForwardAxis(originPoint), zone.bounds[front]);
+
+		bounds[back] = ForwardAxis(originPoint);
+
+		eligibleSides = new bool[4];
+		for(int i = 0; i < 4; i++)
+		{
+			if(i == (int)backSide || SideBlocked(bounds[i], i)) continue;
+			else eligibleSides[i] = true;
+		}
+		return bounds;
+	}
+
+
+	bool CheckAdjacent(Int2 originPoint, int index, out int rightBound, out int leftBound)
+	{
+		rightBound = 0;
+		leftBound = 0;
+
+		for(int b = 0; b < index; b++)
+		{
+			if(Behind(allBounds[b][back], ForwardAxis(originPoint)) && InFront(allBounds[b][front], ForwardAxis(originPoint)))	
+			{
+				if(ToLeft(allBounds[b][right], SideAxis(originPoint)))
+				{
+					if(leftBound == 0 || ToRight(allBounds[b][right], leftBound))
+					{
+						leftBound = allBounds[b][right];
+					}
+				}
+				else if(ToRight(allBounds[b][left], SideAxis(originPoint)))
+				{
+					if(rightBound == 0 || ToLeft(allBounds[b][left], rightBound))
+					{
+						rightBound = allBounds[b][left];
+					}
+				}
+			}
+		}
+
+		if(leftBound != 0 || rightBound != 0) return false;
+		else return true;
+	}
+
+	bool CheckForward(Int2 originPoint, int[] bounds, int index, out int closest)
+	{
+		closest = 0;
+		for(int b = 0; b < index; b++)
+		{
+			if(	ToRight(allBounds[b][right], bounds[left]) 	&& ToLeft(allBounds[b][right], bounds[right]) ||
+				ToRight(allBounds[b][left], bounds[left]) 	&& ToLeft(allBounds[b][left], bounds[right])  ||
+				ToLeft(allBounds[b][left], bounds[left]) 	&& ToRight(allBounds[b][right], bounds[right]))
+			{
+				if(InFront(allBounds[b][back], ForwardAxis(originPoint)) && (closest == 0 || Behind(allBounds[b][back], closest)))
+					closest = allBounds[b][back];
+			}	
+		}
+		if(closest == 0) return false;
+		else return true;
+	}
+
+
+	//	Check if side is up against another side
+	bool SideBlocked(int bound, int side)
+	{
+		int otherSide = (int)Zone.Opposite(side);
+
+		foreach(int[] bounds in allBounds)
+		{
+			if(bounds[otherSide] == bound) return true;
+		}
+		return false;
+	}
 	int MostOpenSide(int[] bounds, bool[] eligibleSides)
 	{
 		int farthestSide = 0;
@@ -117,6 +304,18 @@ public class LSystem
 		return farthestSide;
 	}
 
+	
+	int RandomRange(int a, int b, bool large = false)
+	{
+		float noise = noiseGen.GetNoise01(a, b);
+		if(large) noise = Mathf.Lerp(0.5f, 1f, noise);
+
+		if(a < b)
+			return Mathf.RoundToInt(a + ((b - a) * noise));
+		else
+			return Mathf.RoundToInt(b + ((a - b) * (1 - noise)));
+	}
+
 	Int2 RandomPointOnSide(int side, int[] bounds)
 	{
 		if(side < 2)
@@ -124,152 +323,12 @@ public class LSystem
 		else
 			return new Int2(RandomRange(bounds[0], bounds[1]), bounds[side]);
 	}
-
-	int[] GenerateRoom(Zone.Side originSide, Int2 originPoint, int index, out bool[] eligibleSides)
+	
+	Vector3 MatrixToGlobal(Int2 local)
 	{
-		int side = (int)originSide;
-
-		bool horizontalFacing = side < 2;
-
-		//	Set adjacent sides depending on origin side
-		int adjacentA;
-		int indexA = horizontalFacing ? 2 : 0;
-		int adjacentB;
-		int indexB = horizontalFacing ? 3 : 1;
-
-		//	Set outward side - opposide to origin side
-		int outward;
-		int indexO = Zone.Opposite(side);
-
-		//	Set adjacent axis - perpendicular to origin side
-		int adjacentAxis = horizontalFacing ? originPoint.z : originPoint.x;
-		//	Set outward access
-		int outwardAxis = horizontalFacing ? originPoint.x : originPoint.z;
-
-		int[] bounds = new int[4];
-
-		//	Check whether objects exist perpendicular to origin point and choose width
-		CheckAdjacency(out adjacentA, out adjacentB, index);
-
-		//	Assign bounds width
-		bounds[indexA] = adjacentA == 0 ? RandomRange(adjacentAxis, zone.bounds[indexA]) : adjacentA;
-		bounds[indexB] = adjacentB == 0 ? RandomRange(adjacentAxis, zone.bounds[indexB]) : adjacentB;
-
-		//	Check if objects are blocking the chosen width outward from the origin side
-		outward = CheckOutward(bounds, index);
-
-		//	Assign bounds origin and outward limit
-		bounds[indexO] = outward == 0 ? RandomRange(outwardAxis, zone.bounds[indexO]) : outward;
-		bounds[side] = outwardAxis;
-
-		eligibleSides = new bool[4];
-		for(int i = 0; i < 4; i++)
-		{
-			if(i == (int)originSide || SideBlocked(bounds[i], i)) continue;
-			else eligibleSides[i] = true;
-		}
-
-		return bounds;
-	}
-
-	void CheckAdjacency(out int topClosest, out int bottomClosest, int index)
-	{
-		topClosest = 0;
-		bottomClosest = 0;
-
-		bool verticalAdjacency = (int)originSides[index] < 2;
-
-		//	Values to check if adjacent
-		int c1 = verticalAdjacency ? 0 : 2;
-		int c2 = verticalAdjacency ? 1 : 3;
-
-		//	Values for assignment checks
-		int a1 = verticalAdjacency ? 2 : 0;
-		int a2 = verticalAdjacency ? 3 : 1;
-
-		//	Origin point to check if adjacent
-		int originPointC = verticalAdjacency ? originPoints[index].x : originPoints[index].z;
-		//	Origin point for assignment
-		int originPointA = verticalAdjacency ? originPoints[index].z : originPoints[index].x;
-
-		for(int b = 0; b < index; b++)
-		{
-			//	Check if other bounds is adjacent to point
-			if(allBounds[b][c2] <= originPointC && allBounds[b][c1] >= originPointC)		
-			{
-				//	Check which side of point other bounds is on and assign max bounds
-				if(allBounds[b][a2] > originPointA)
-				{
-					if(topClosest == 0 || allBounds[b][a2] < topClosest)
-						topClosest = allBounds[b][a2];
-				}
-				else if(allBounds[b][a1] < originPointA)
-				{
-					if(bottomClosest == 0 || allBounds[b][a1] > bottomClosest)
-						bottomClosest = allBounds[b][a1];
-				}
-			}
-		}
-	}
-
-	int CheckOutward(int[] bounds, int index)
-	{
-		int closest = 0;
-
-		int side = (int)originSides[index];
-
-		bool horizontalBlocker = (int)originSides[index] < 2;
-
-		int point = horizontalBlocker ? originPoints[index].x : originPoints[index].z;
-
-		int c1 = horizontalBlocker ? 2 : 0;
-		int c2 = horizontalBlocker ? 3 : 1;
-		
-		for(int b = 0; b < index; b++)
-		{
-			if(BoundsBlockingBounds(allBounds[b][c2], allBounds[b][c1], bounds[c2], bounds[c1]))	//	Check other left and right against current left and right bounds
-			{
-				switch(originSides[index])
-				{
-					case Zone.Side.RIGHT:
-					case Zone.Side.TOP:
-						if(allBounds[b][side] < point)
-							if(closest == 0 || allBounds[b][side] > closest)
-								closest = allBounds[b][side];
-						break;
-					case Zone.Side.LEFT:
-					case Zone.Side.BOTTOM:
-						if(allBounds[b][side] > point)
-							if(closest == 0 || allBounds[b][side] < closest)
-								closest = allBounds[b][side];
-						break;
-				}
-			}
-		}
-
-		return closest;
-	}
-
-	//	Check if a is overlapping b
-	bool BoundsBlockingBounds(int a1, int a2, int b1, int b2)
-	{
-		if(	(a1 > b1 && a1 < b2)||
-			(a2 > b1 && a2 < b2)||
-			(a1 < b1 && a2 > b2) ) return true;
-
-		return false;
-	}
-
-	//	Check if side is up against another side
-	bool SideBlocked(int bound, int side)
-	{
-		int otherSide = (int)Zone.Opposite(side);
-
-		foreach(int[] bounds in allBounds)
-		{
-			if(bounds[otherSide] == bound) return true;
-		}
-		return false;
+		return new Vector3(	(int)POI.position.x + (zone.x*World.chunkSize) + local.x,
+							0,
+							(int)POI.position.z + (zone.z*World.chunkSize) + local.z);
 	}
 
 	void DrawRoom(int[] bounds)
@@ -285,96 +344,6 @@ public class LSystem
 			zone.blockMatrix[bounds[1], z] = 1;
 			zone.blockMatrix[bounds[0], z] = 1;
 		}
-	}
-
-	void DebugRooms(int index)
-	{
-		int[] bounds = allBounds[index];
-		int middleX = ((bounds[0] - bounds[1]) / 2) + bounds[1];
-		int middleZ = ((bounds[2] - bounds[3]) / 2) + bounds[3];
-
-		zone.blockMatrix[middleX, middleZ] = 2;
-
-		zone.blockMatrix[originPoints[index].x, originPoints[index].z] = 3;
-	}
-
-	
-	int RandomRange(int a, int b, bool large = false)
-	{
-		float noise = noiseGen.GetNoise01(a, b);
-		if(large) noise = Mathf.Lerp(0.5f, 1f, noise);
-
-		if(a < b)
-			return Mathf.RoundToInt(a + ((b - a) * noise));
-		else
-			return Mathf.RoundToInt(b + ((a - b) * (1 - noise)));
-	}
-
-
-
-
-	/*void RoadBetweenPoints(Int2 startPoint, Int2 endPoint)
-	{
-		Vector2 differenceVector = new Vector2(endPoint.x, endPoint.z) - new Vector2(startPoint.x, startPoint.z);
-
-		Int2 difference = new Int2((int)differenceVector.x, (int)differenceVector.y);
-
-		int xSign = (int)Mathf.Sign(difference.x);
-		int zSign = (int)Mathf.Sign(difference.z);
-
-		int zRoad = startPoint.z;
-
-		for(int xRoad = startPoint.x; DynamicCompare(xRoad, endPoint.x, xSign); xRoad+=xSign)
-		{
-			if(xRoad == Mathf.Abs(difference.x / 2))
-			{
-				for(zRoad = startPoint.z; DynamicCompare(zRoad, endPoint.z, zSign); zRoad+=zSign)
-				{
-					zone.blockMatrix[xRoad,zRoad] = 1;
-				}
-				zRoad -= zSign;
-			}
-			else
-			{
-				zone.blockMatrix[xRoad,zRoad] = 1;
-			}
-		}
-	}
-	
-
-	bool DynamicCompare(int value, int compareTo, int sign)
-	{
-		if(sign > 0)
-			return value <= compareTo;
-		else
-			return value >= compareTo;
-	}*/
-
-	Int2 RandomPointAtEdge(Zone.Side edge, int width, int height, float noise)
-	{
-		int top = height-1;
-		int right = width-1;
-		//	Pick starting point for main building
-		switch(edge)
-		{
-			case Zone.Side.BOTTOM:
-				return new Int2(Mathf.FloorToInt(right * noise), 0);
-			case Zone.Side.TOP:
-				return new Int2(Mathf.FloorToInt(right * noise), top);
-			case Zone.Side.LEFT:
-				return new Int2(0, Mathf.FloorToInt(top * noise));
-			case Zone.Side.RIGHT:
-				return new Int2(right, Mathf.FloorToInt(top * noise));
-			default:
-				return new Int2(0, 0);
-		}
-	}
-	
-	Vector3 MatrixToGlobal(Int2 local)
-	{
-		return new Vector3(	(int)POI.position.x + (zone.x*World.chunkSize) + local.x,
-							0,
-							(int)POI.position.z + (zone.z*World.chunkSize) + local.z);
 	}
 
 	void SetColumnMaps()
@@ -396,5 +365,16 @@ public class LSystem
 						column.POIMap[cx,cz] = zone.blockMatrix[mx,mz];
 					}
 			}
+	}
+
+	void DebugRooms(int index)
+	{
+		int[] bounds = allBounds[index];
+		int middleX = ((bounds[0] - bounds[1]) / 2) + bounds[1];
+		int middleZ = ((bounds[2] - bounds[3]) / 2) + bounds[3];
+
+		zone.blockMatrix[middleX, middleZ] = 2;
+
+		zone.blockMatrix[originPoints[index].x, originPoints[index].z] = 3;
 	}
 }
